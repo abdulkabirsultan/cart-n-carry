f24d32c118f223caed7cbbe7ddafcc1cf495390c
Abdulkabir sultan <abdulkabirsultan@gmail.com>
2022-09-13 01:55:23 +0100

    Add folder populate

--- server/populate/populate.js
+++ server/populate/populate.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import Products from './models/Products.js';
+import Products from '../models/Products.js';
 import populate from './populated.js';
 const app = express();
 const start = async () => {
   try {
     await mongoose.connect('mongodb://localhost:27017/ecommerceDB');
     console.log('database connected');
+    await Products.deleteMany({});
     await Products.insertMany(populate);
-    // await Products.deleteMany({});
     await app.listen(2000, console.log('server started'));
     process.exit(0);
   } catch (error) {
