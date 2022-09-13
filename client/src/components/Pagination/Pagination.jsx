085b545896d95ba2e3ac1b715d736aed2e210392
Abdulkabir sultan <abdulkabirsultan@gmail.com>
2022-09-14 00:00:50 +0100

    make pagination responsive

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -8,29 +8,31 @@ const Pagination = () => {
   const paginateCount = paginateFunc(products);
   const [index, setIndex] = useState(1);
   return (
-    <div className='flex justify-center items-center my-6'>
+    <div className='flex justify-center flex-wrap relative items-center my-6'>
       <span
-        className='btn btn-xs btn-circle mr-5'
+        className='btn absolute md:static left-3 btn-xs btn-circle mr-6'
         onClick={() => setIndex(index <= 1 ? paginateCount.length : index - 1)}
       >
         <FaChevronLeft />
       </span>
-      {paginateCount.map((p, i) => {
-        const num = ++i;
-        return (
-          <Button
-            key={num}
-            className={`mr-2 btn-sm ${
-              num === index && 'bg-[#33abdf] hover:bg-[#3ABFF8]'
-            }`}
-            onClick={() => setIndex(num)}
-          >
-            {p + 1}
-          </Button>
-        );
-      })}
+      <div className='mx-9 md:mx-0 flex flex-wrap justify-center'>
+        {paginateCount.map((p, i) => {
+          const num = ++i;
+          return (
+            <Button
+              key={num}
+              className={`mr-2 mb-3 md:mb-0 btn-sm ${
+                num === index && 'bg-[#33abdf] hover:bg-[#3ABFF8]'
+              }`}
+              onClick={() => setIndex(num)}
+            >
+              {p + 1}
+            </Button>
+          );
+        })}
+      </div>
       <span
-        className='btn btn-xs btn-circle ml-3'
+        className='btn absolute md:static right-3 btn-xs btn-circle ml-4'
         onClick={() => setIndex(index >= paginateCount.length ? 1 : index + 1)}
       >
         <FaChevronRight />
