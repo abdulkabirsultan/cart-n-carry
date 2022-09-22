aa57220a4e05424218e4052666a4712e1ddd09aa
Abdulkabir sultan <abdulkabirsultan@gmail.com>
2022-09-22 17:27:16 +0100

    add cart functionality

--- client/src/Pages/SharedLayout.jsx
+++ client/src/Pages/SharedLayout.jsx
@@ -1,8 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Outlet } from 'react-router-dom';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar/Navbar';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotal, getCartProducts } from '../Features/cartSlice';
 
 const SharedLayout = () => {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCartProducts());
+  }, []);
+  useEffect(() => {
+    dispatch(calculateTotal());
+  }, [cartItems]);
+
   return (
     <>
       <Navbar />
