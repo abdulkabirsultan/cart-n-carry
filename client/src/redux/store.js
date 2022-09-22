947edcc44f61ee86d615444ceda5f8c87f714d8e
Abdulkabir sultan <abdulkabirsultan@gmail.com>
2022-09-22 17:19:26 +0100

    add cart reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { save, load } from 'redux-localstorage-simple';
+import cartReducer from '../Features/cartSlice';
+import modalReducer from '../Features/modalSlice';
 import productReducer from '../Features/productSlice';
 export const store = configureStore({
-  reducer: { products: productReducer },
+  reducer: { products: productReducer, cart: cartReducer, modal: modalReducer },
+  middleware: (gdM) =>
+    gdM().concat(save({ states: ['cart'], namespace: 'cart' })),
+
+  preloadedState: load({
+    states: ['cart'],
+    namespace: 'cart',
+  }),
 });
